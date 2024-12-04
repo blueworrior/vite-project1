1732fc70fe1888a029a31f8edf018ae60200d39f
blueworrior <ssjblueworrior@gmail.com>
2024-12-04 23:59:04 +0500

    s

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,53 +7,68 @@ import { Link } from "react-router-dom";
 
 const Navbar = (props) => {
 
+  const [menu, setmenu] = useState(150)
+
+  const menuclick = (() => {
+    if(menu === 150)
+      setmenu(0)
+    else
+      setmenu(150)
+  })
+
   return (
     <>
       <style>
         <body style={props.mode}></body>
       </style>
       <div style={props.mode} className="navbar">
-        <ul id="nav">
-          <li id="wb">{props.title}</li>
-          <li id="th">
-            <div
-              style={props.mode}
-              onClick={props.handleMode}
-              className="dlModebck"
-            >
-              <div className="dlMode" style={props.modecircle}></div>
-              <div className="imgdiv">
-                <img src={props.sun} alt="" id="sun" />
-                <img src={props.moon} alt="" id="moon" />
+          <ul id="nav">
+            <li id="wb">{props.title}</li>
+            <li id="th">
+              <div
+                style={props.mode}
+                onClick={props.handleMode}
+                className="dlModebck">
+                <div className="dlMode" style={props.modecircle}></div>
+                <div className="imgdiv">
+                  <img src={props.sun} alt="" id="sun" />
+                  <img src={props.moon} alt="" id="moon" />
+                </div>
               </div>
-            </div>
-          </li>
-          <li>
-            <Link style={props.mode} id="li" to={"/vite-project1"}>Home</Link>
-          </li>
-          <li>
-          <Link style={props.mode} id="li" to={"/vite-project1"}>Trending</Link>
-          </li>
-          <li>
-          <Link style={props.mode} id="li" to={"/vite-project1"}>Category</Link>
-          </li>
-          <li>
-            <Link style={props.mode} id="li" to={"/vite-project1/about"}>About</Link>
-          </li>
+            </li>
+        <img onClick={menuclick} style={props.mode2} src="menubar.png" alt="" id="menuimg"/>
 
-          <li id="thmli">
-            <select style={props.mode} onClick={props.handleTheme} value={props.theme} onChange={e=>props.setTheme(e.target.value)} name="" id="thm">
-              <option>OG</option>
-              <option>Green</option>
-              <option>Blue</option>
-              <option>Red</option>
-            </select>
-          </li>
-          <li>
-            <Link style={props.mode2} to={"/vite-project1/login"} id="si">Sign in</Link>
-          </li>
-        </ul>
+        <div style={{
+          ...props.mode,
+          left: menu,
+          }} className="menubar">
+            <li>
+              <Link style={props.mode} id="li" to={"/"}>Home</Link>
+            </li>
+            <li>
+            <Link style={props.mode} id="li" to={""}>Trending</Link>
+            </li>
+            <li>
+            <Link style={props.mode} id="li" to={""}>Category</Link>
+            </li>
+            <li>
+              <Link style={props.mode} id="li" to={"/about"}>About</Link>
+            </li>
+
+            <li id="thmli">
+              <select style={props.mode} onClick={props.handleTheme} value={props.theme} onChange={e=>props.setTheme(e.target.value)} name="" id="thm">
+                <option>OG</option>
+                <option>Green</option>
+                <option>Blue</option>
+                <option>Red</option>
+              </select>
+            </li>
+            <li>
+              <Link style={props.mode2} to={"/login"} id="si">Sign in</Link>
+            </li>
       </div>
+          </ul>
+        </div>
     </>
   );
 };
